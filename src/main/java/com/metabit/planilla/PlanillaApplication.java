f3e3bee31d6fed2ecd5e3abb674f2d447352b5de
edwinpalacio5 <ap16014@ues.edu.sv>
2020-04-26 22:55:58 -0600

    commit inicial. Estructura del proyecto

---
+++ src/main/java/com/metabit/planilla/PlanillaApplication.java
@@ -0,0 +1,13 @@
+package com.metabit.planilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanillaApplication.class, args);
+	}
+
+}
