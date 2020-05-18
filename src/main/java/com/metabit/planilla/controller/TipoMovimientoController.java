a5582e33a5135d5bdd5cac9e93ea04a61474fd54
edwinpalacio5 <ap16014@ues.edu.sv>
2020-05-17 20:29:36 -0600

    Crear/Editar tipo de movimiento

--- src/main/java/com/metabit/planilla/controller/TipoMovimientoController.java
+++ src/main/java/com/metabit/planilla/controller/TipoMovimientoController.java
@@ -1,5 +1,9 @@
 package com.metabit.planilla.controller;
 
+import java.util.Optional;
+
+import javax.validation.Valid;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,11 +11,17 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.metabit.planilla.entity.Puesto;
+import com.metabit.planilla.entity.TipoMovimiento;
 import com.metabit.planilla.service.TipoMovimientoService;
 
 @Controller
@@ -42,6 +52,44 @@ public class TipoMovimientoController {
 		model.addAttribute("update_success", update_success);
 		model.addAttribute("delete_success", delete_success);
 		return mav;
+	}
+	
+	@PreAuthorize("hasAuthority('TIPOMOVIMIENTO_CREATE') or hasAuthority('TIPOMOVIMIENTO_EDIT')")
+	@GetMapping(path ={"/form-tipo-movimiento" ,"/form-tipo-movimiento/{id}"})
+	public ModelAndView create(@PathVariable("id") Optional<Integer> id) {
+		ModelAndView mav = new ModelAndView(CREATE_VIEW);
+		if(id.isPresent()) {
+			//actualizamos 
+			Integer idTipoMovimiento = Integer.valueOf(id.get());
+			TipoMovimiento tipoMovimiento = tipoMovimientoService.getTipoMovimiento(idTipoMovimiento);
+			mav.addObject("tipoMovimientoEntity", tipoMovimiento);
+		}else {
+			// tipoMovimeinto empty para crear un nuevo tipoMovimiento
+			mav.addObject("tipoMovimientoEntity", new TipoMovimiento());
+		}
+		return mav;
+	}
+	
+	// método para recibir el post del formulario de crear/editar 
+	@PreAuthorize("hasAuthority('TIPOMOVIMIENTO_CREATE') or hasAuthority('TIPOMOVIMIENTO_EDIT')")
+	@PostMapping("/form-post")
+	public String createUpdatePost(@Valid @ModelAttribute("tipoMovimientoEntity") TipoMovimiento tipoMovimiento, BindingResult bindingResult) {
+		LOGGER.info("TIPO MOVIMIENTO : " + tipoMovimiento);
+		if (bindingResult.hasErrors()) {
+			//retornamos al layout
+			return CREATE_VIEW;
+		} else {
+			//creamos y retornamos a listado de tipos de movimiento
+			if(tipoMovimiento.getIdMovimiento() == 0) {
+				tipoMovimientoService.storeTipoMovimiento(tipoMovimiento);
+				return "redirect:/tipo-movimiento/index?store_success=true";
+			}else {
+				tipoMovimiento.setTipoMovimientoHabilitado(true);
+				tipoMovimientoService.storeTipoMovimiento(tipoMovimiento);
+				return "redirect:/tipo-movimiento/index?update_success=true";
+			}
+			
+		}
 	}
 
 }
