1e88868b7933c23d9175274a8b226a8c994250e8
Ricardo Estupinian <ricaeslo@gmail.com>
2020-06-19 23:51:55 -0600

    Gestion de departamento, finalizada

--- src/main/java/com/metabit/planilla/service/DireccionService.java
+++ src/main/java/com/metabit/planilla/service/DireccionService.java
@@ -2,10 +2,12 @@ package com.metabit.planilla.service;
 
 import com.metabit.planilla.entity.Direccion;
 
+import java.util.List;
+
 public interface DireccionService {
 
     public abstract Direccion addDirection(Direccion d);
     public abstract Direccion updateDirection(Direccion d);
     public abstract Direccion getDirection(int id);
-
+    public abstract List<Direccion> getAllDirecciones();
 }
